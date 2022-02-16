5e05c444936aef283a25cb351b4e7f411b9f9060
RossettaBitbucket <izzyshocks@gmail.com>
2022-02-16 19:25:22 +0000

    ...

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -13,8 +13,9 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal");
+        float steerAmount = Input.GetAxis("Horizontal") *steerSpeed* Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical")*moveSpeed* Time.deltaTime;
          transform.Rotate(0,0,-steerAmount);
-         transform.Translate(0,moveSpeed,0);
+         transform.Translate(0,moveAmount,0);
     }
 }
